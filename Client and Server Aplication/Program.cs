6c34b16a19d2911cb9263dc0c1cd19c47e26eef2
benjaminRofB <51160128+benjaminRofB@users.noreply.github.com>
2019-07-11 01:33:20 +0300

    )

---
+++ Client and Server Aplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            socket.Connect("127.0.0.1", 904);
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            socket.Send(buffer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static void Main(string[] args)
+        {
+            socket.Bind(new IPEndPoint(IPAddress.Any, 904));
+            socket.Listen(5);
+            Socket client = socket.Accept();
+            Console.WriteLine("New connection");
+            byte[] buffer = new byte[1024];
+            client.Receive(buffer);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer));
+            Console.ReadLine();
+        }
+    }
+}
